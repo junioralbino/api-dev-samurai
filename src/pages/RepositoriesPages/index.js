c254be563251dbcf265b2cdb9930aabeb38b2c81
Suporte <junioralbino741@gmail.com>
2022-08-04 17:51:49 -0300

    Criando novo respositoria dinamico

--- src/pages/RepositoriesPages/index.js
+++ src/pages/RepositoriesPages/index.js
@@ -23,36 +23,42 @@ const RepositoriesPage = () => {
   // eslint-disable-next-line no-unused-vars
   const repositories = [
     {
+      id: 1,
       name: 'Repo 1',
       description: 'Descrição',
       html_url: 'https://devsamurai.com.br',
       language: 'JavaScript',
     },
     {
+      id: 2,
       name: 'Repo 2',
       description: 'Descrição',
       html_url: 'https://devsamurai.com.br',
       language: 'JavaScript',
     },
     {
+      id: 3,
       name: 'Repo 3',
       description: 'Descrição',
       html_url: 'https://devsamurai.com.br',
       language: 'PHP',
     },
     {
+      id: 4,
       name: 'Repo 4',
       description: 'Descrição',
       html_url: 'https://devsamurai.com.br',
       language: null,
     },
     {
+      id: 5,
       name: 'Repo 5',
       description: 'Descrição',
       html_url: 'https://devsamurai.com.br',
       language: 'TypeScript',
     },
     {
+      id: 6,
       name: 'Repo 6',
       description: 'Descrição',
       html_url: 'https://devsamurai.com.br',
@@ -69,7 +75,7 @@ const RepositoriesPage = () => {
         <Filter languages={languages} />
       </Sidebar>
       <Main>
-        <Repositories />
+        <Repositories repositories={repositories} />
       </Main>
     </Container>
   );
