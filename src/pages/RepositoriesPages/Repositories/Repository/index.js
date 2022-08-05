4afcd855a66987d70abf7ab39704d489fc006146
Suporte <junioralbino741@gmail.com>
2022-08-05 17:47:36 -0300

    quase finalizado os dados dinamico do git

--- src/pages/RepositoriesPages/Repositories/Repository/index.js
+++ src/pages/RepositoriesPages/Repositories/Repository/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Name, Description, Footer, Lang, Link } from './styles';
 
-function Repository() {
+function Repository({ repository }) {
   return (
     <Container color="#f1c40f">
-       <Name>Repository Name</Name>
-       <Description>Repository Description</Description>
+       <Name>{ repository.name }</Name>
+       <Description>{repository.description}</Description>
        <Footer color="#f1c40f">
-         <Lang>Repository Lang</Lang>
-         <Link href="https://devsamurai.com.br" target="_blank">Ver</Link>
+         <Lang>{repository.language}</Lang>
+         <Link href={ repository.html_url } target="_blank">Ver</Link>
        </Footer>
     </Container>
   )
+}
+
+Repository.PropTypes = {
+  repository:  PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired,
+    language: PropTypes.string
+  }).isRequired
 }
 
 export default Repository
